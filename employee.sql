f9a0da1db12a1a35e375e828d293eccda8c113d4
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-07 15:36:27 -0800

    EmployeeSQL Complete

--- employee.sql
+++ employee.sql
@@ -1,2 +1,22 @@
 
-SELECT * FROM employee
+-- SELECT * FROM employee;
+
+-- SELECT first_name, last_name FROM employee
+-- WHERE city = 'Calgary';
+
+-- SELECT MIN(birth_date)
+-- FROM employee;
+
+-- SELECT MAX(birth_date)
+-- FROM employee;
+
+-- SELECT * FROM employee
+-- WHERE first_name = 'Nancy';
+
+-- SELECT * FROM employee
+-- WHERE reports_to = '2';
+
+-- SELECT COUNT(employee.city)
+-- FROM employee
+-- WHERE city = 'Lethbridge';
+
