fa99fc4f1995bae65fe4a5b47c1b5473dc053e0f
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-07 15:19:55 -0800

    ArtistSQL Completed

---
+++ artist.sql
@@ -0,0 +1,21 @@
+
+-- SELECT * FROM artist
+
+-- INSERT INTO artist (name)
+-- 	VALUES ('Hozier'),
+--   ('Alec Benjamin'),
+--   ('Lana Del Rey');
+
+-- SELECT * FROM artist
+-- ORDER BY name DESC
+-- LIMIT 10;
+
+-- SELECT * FROM artist
+-- ORDER BY name ASC
+-- LIMIT 5;
+
+-- SELECT * FROM artist
+-- WHERE name LIKE 'Black%';
+
+-- SELECT * FROM artist
+-- WHERE name LIKE '%Black%';
